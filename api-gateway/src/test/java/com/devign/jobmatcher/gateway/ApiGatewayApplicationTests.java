aa9eb97939f62fcef88619e569476f48b2eef968
Sari Hammad <sari_hammad@hotmail.com>
2025-05-22 13:41:10 -0400

    feat: init project structure

---
+++ api-gateway/src/test/java/com/devign/jobmatcher/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devign.jobmatcher.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ job-service/src/test/java/com/devign/jobmatcher/job/JobServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devign.jobmatcher.job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
